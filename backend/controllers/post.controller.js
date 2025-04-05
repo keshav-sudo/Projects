3dba60d9419f2ed0079b178011c172dc37886346
keshav-sudo <thesharmakeshav@gmail.com>
2025-04-05 20:44:39 +0530

    post route

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -59,14 +59,50 @@ export const createPost = async (req, res) => {
   }
 };
 
+export const getallpost = async (req , res)=>{
+    try {
+        const posts = await Post.find()
+        .sort({createdAt : -1})
+        .populate({
+            path: "user",
+            select: "-password"
+        })
+        .populate({
+            path: "comments.user",
+            select: "-password"
+        });
 
-
-
-
+        if(posts.length ==  0){
+            res.status(200).json([]);
+        }
+        res.status(200).json(posts);
+    } catch (error) {
+        console.log("Error in getAllPosts controller: ", error);
+		res.status(500).json({ error: "Internal server error" })
+    }
+}
 
 
 export const deletePost = async (req , res) =>{
-    res.send("hit delete post route")
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post){
+            return res.status(401).json({message : "post does'nt find"})
+        }
+        if(post.user.toString() !== req.user._id.toString()){
+            res.status(401).json({msg : "you are not authorised to delete this post"})
+        }
+        if(post.Img){
+            const Imgid = post.Img.split("/").pop().split(".")[0];
+            await cloudinary.uploader.destroy(Imgid);
+        }
+
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: "Post deleted successfully" });
+    } catch (error) {
+        console.log("Error in deletePost controller: ", error);
+		res.status(500).json({ error: "Internal server error" });
+    }
 }
 
 
@@ -92,9 +128,6 @@ export const likeunlikepost = async (req , res)=>{
 
 
 
-export const getallpost = async (req , res)=>{
-    res.send("hit comment route")
-}
 
 
 export const getlikedpost = async (req , res)=>{
--- backend/routes/post.Routes.js
+++ backend/routes/post.Routes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { protect } from "../middleware/protect.js";
-import { commentOnPost, createPost, deletePost, getfollowingpost, getlikedpost, getuserpost, likeunlikepost } from "../controllers/post.controller.js";
+import { commentOnPost, createPost, deletePost, getallpost, getfollowingpost, getlikedpost, getuserpost, likeunlikepost } from "../controllers/post.controller.js";
 import { upload } from "../middleware/multer.js";
 
 const postRouter = express.Router();
 
 postRouter.post("/create", protect, upload.single("Img"), createPost);
-postRouter.get("/delete" , deletePost )
+postRouter.get("/getall" ,  getallpost);
+postRouter.delete("/:id" ,protect, deletePost )
 postRouter.get("/comment" , commentOnPost  )
 postRouter.get("/likeunlike" , likeunlikepost  )
 postRouter.get("/getliked" , getlikedpost  )
